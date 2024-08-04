c712db8b90a2d8bc24a9a7c54288b1ac7ee2857f
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-08-04 08:39:22 -0300

    feat: Implement cat-file command

--- data.py
+++ data.py
@@ -3,12 +3,16 @@ import os
 
 GIT_DIR = '.ugit'
 
-def init ():
-    os.makedirs (GIT_DIR)
-    os.makedirs (f'{GIT_DIR}/objects')
+def init():
+    os.makedirs(GIT_DIR)
+    os.makedirs(f'{GIT_DIR}/objects')
 
-def hash_object (data):
-    oid = hashlib.sha1 (data).hexdigest ()
-    with open (f'{GIT_DIR}/objects/{oid}', 'wb') as out:
-        out.write (data)
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write(data)
     return oid
+
+def get_object(oid):
+    with open(f'{GIT_DIR}/objects/{oid}', 'rb') as f:
+        return f.read()
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
-setup (name = 'ugit',
-       version = '1.0',
-       packages = ['ugit'],
-       entry_points = {
-           'console_scripts' : [
-               'ugit = ugit.cli:main'
-           ]
-       })
+setup(
+    name='ugit',
+    version='1.0',
+    packages=['ugit'],
+    entry_points={
+        'console_scripts': [
+            'ugit = ugit.cli:main'
+        ]
+    }
+)
